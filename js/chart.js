f0aeeaaa2514e9c23b957af83623fc93151e93ae
ALEJANDRA ALTAMIRANO <a.altamirano0208@gmail.com>
2023-07-27 16:03:51 -0700

    updated chart.js

--- js/chart.js
+++ js/chart.js
@@ -10,7 +10,46 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
-function renderChart() {
-}
-
-renderChart();
+class AppState {
+    constructor() {
+      this.allProducts = [];
+    }
+  
+    loadVoteData() {
+      const savedVoteData = localStorage.getItem('voteData');
+      if (savedVoteData) {
+        this.allProducts = JSON.parse(savedVoteData);
+      }
+    }
+  }
+  
+  function renderChart() {
+    const canvasElem = document.getElementById('chart');
+    const appState = new AppState();
+    appState.loadVoteData();
+  
+    const chartData = {
+      labels: appState.allProducts.map(product => product.name),
+      datasets: [
+        {
+          label: 'Votes',
+          data: appState.allProducts.map(product => product.voteCount),
+          backgroundColor: 'rgba(75, 192, 192, 0.2)', // You can customize the chart colors here
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1,
+        },
+      ],
+    };
+  
+    const chartConfig = {
+      type: 'bar', 
+      data: chartData,
+      options: {
+      },
+    };
+  
+    const chartContext = canvasElem.getContext('2d');
+    new Chart(chartContext, chartConfig);
+  }
+ 
+  renderChart();
